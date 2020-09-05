d0fb4138a83c666ed388011278038621210d1252
kenichi-shimada <kenichi.shimada@gmail.com>
2020-09-05 14:39:37 -0400

    modify find_ess_genes

--- 03_find_essential_genes/find_ess_genes.r
+++ 03_find_essential_genes/find_ess_genes.r
@@ -1,3 +1,5 @@
+setwd(src_dir);source("functions.r")
+
 k <- as.numeric(commandArgs(TRUE))[1] # 1-12
 
 c1 <- seq(0,1,.2)[k]
@@ -24,61 +26,7 @@ names(rths) <- paste0("r",qtl2-qtl)
 r1 <- range(unlist(qths),na.rm=T)
 r2 <- range(unlist(rths),na.rm=T)
 
-## Identification of essential and growth-suppressing genes
-find.threshold <- function(dep.scores,#breaks=100,
-	side.fit=c("upper","lower"),side.thres=c("upper","lower"),
-	ps=c(1e-2,1e-3,1e-5),
-	plot=TRUE,...){
 
-		# xh <- hist(dep.scores,col="lightblue",border=NA,breaks=seq.br,plot=TRUE,...)
-		d <- density(dep.scores)
-		mod <- d$x[which.max(d$y)]
-
-		# if(0){
-		# 	seq.br <- seq(xlim[1],xlim[2],length=breaks)
-		# 	nl <- length(xh$breaks)
-		# 	br <- (xh$breaks[-1]+xh$breaks[-nl])/2
-
-		# 	ct <- xh$counts
-		# 	names(ct) <- br
-		# }else{
-		# 	ct <- d$y
-		# 	names(ct) <- d$x
-		# }
-
-		# mod <- as.numeric(names(which.max(ct)))
-
-		if(side.fit[1]=="upper"){
-			val <- (dep.scores-mod)
-			val <- val[val > 0] 
-			sd <- sqrt(sum(val^2)/(length(val)-1))
-			names(ths) <- ps
-		}else{
-			val <- (dep.scores-mod)
-			val <- val[val < 0] 
-			sd <- sqrt(sum(val^2)/(length(val)-1))
-			ths <- qnorm(1-ps,mean=mod,sd=sd)
-			names(ths) <- ps
-		}
-
-		if(side.thres[1]=="upper"){
-			ths <- qnorm(1-ps,mean=mod,sd=sd)
-			col <- "pink"
-		}else{
-			ths <- mod * 2 - qnorm(1-ps,mean=mod,sd=sd)
-			col <- "lightblue"
-		}
-		if(plot){
-			plot(d$x,d$y,type="n",...)
-			polygon(d$x,d$y,col=col,border=NA)
-			ybr <- dnorm(seq.br,mean=mod,sd=sd)
-			f <- max(d$y)/max(ybr)
-			lines(seq.br,f*dnorm(seq.br,mean=mod,sd=sd),col=4,lwd=2)
-			abline(v=ths,col=2)
-		}
-
-		return(ths)
-}
 
 ## create directories for saving plots
 setwd(plot_dir);setwd("03_histogram_essential_genes")
